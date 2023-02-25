931a6ba068a7c8c8fa7d4d4f58f4d08ae71fcc7c
JSander72 <jamesearlsanders2@outlook.com>
2023-02-25 14:30:09 -0500

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-var gameName = "TIC TAC TOE"
+const player_x_class = "X"
+const player_o_class = "O"
+const winning_combinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 
-console.log()
+]
+
+
